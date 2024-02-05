58f504d1ea12df75d21b7a651205ab46c8c99641
juancodev <montillasanchezjuancarlos@gmail.com>
2024-02-05 00:39:55 -0400

    [⚡ADD] initialize project

---
+++ src/components/form/Form.jsx
@@ -0,0 +1,172 @@
+import { useState } from "react"
+
+const Form = () => {
+  const [typeForm, setTypeForm] = useState(true);
+
+  if (typeForm) {
+    return (
+      <>
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <img
+              className="mx-auto h-10 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              alt="Your Company"
+            />
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
+              Inicio de Sesión
+            </h2>
+          </div>
+
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form className="space-y-6" action="#" method="POST">
+              <div>
+                <label htmlFor="email" className="block text-sm font-medium leading-6 text-white">
+                  Correo Electrónico
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <div className="flex items-center justify-between">
+                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-white">
+                    Contraseña
+                  </label>
+                  <div className="text-sm">
+                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                      ¿Olvidó su contraseña?
+                    </a>
+                  </div>
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Iniciar Sesión
+                </button>
+              </div>
+            </form>
+
+            <p className="mt-10 text-center text-sm text-gray-500">
+              ¿No eres miembro?{' '}
+              <a className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500 cursor-pointer" onClick={() => { setTypeForm(false) }}>
+                Regístrate aquí
+              </a>
+            </p>
+          </div>
+        </div>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-9 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <img
+              className="mx-auto h-10 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              alt="Your Company"
+            />
+            <h2 className="mt-7 text-center text-2xl font-bold leading-9 tracking-tight text-white">
+              Registro de Sesión
+            </h2>
+          </div>
+
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form className="space-y-6" action="#" method="POST">
+              <div>
+                <label htmlFor="fullName" className="block text-sm font-medium leading-6 text-white">
+                  Nombre Completo
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="fullName"
+                    name="fullName"
+                    type="text"
+                    autoComplete="fullName"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label htmlFor="email" className="block text-sm font-medium leading-6 text-white">
+                  Correo Electrónico
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <div className="flex items-center justify-between">
+                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-white">
+                    Contraseña
+                  </label>
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Registrar
+                </button>
+              </div>
+            </form>
+
+            <p className="mt-10 text-center text-sm text-gray-500">
+              ¿Ya eres miembro?{' '}
+              <a className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500 cursor-pointer" onClick={() => { setTypeForm(true) }}>
+                Inicia Sesión
+              </a>
+            </p>
+          </div>
+        </div>
+      </>
+    )
+  }
+}
+
+export { Form };
---
+++ src/routes.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+// Admin Imports
+import MainDashboard from "views/admin/default";
+import Services from "views/admin/services";
+import Profile from "views/admin/profile";
+import Reports from "views/admin/reports";
+// import RTLDefault from "views/rtl/default";
+
+// Auth Imports
+import SignIn from "views/auth/SignIn";
+
+// Icon Imports
+import {
+  MdHome,
+  MdOutlineShoppingCart,
+  MdBarChart,
+  MdPerson,
+  MdLock,
+} from "react-icons/md";
+
+const routes = [{
+    name: "Dashboard",
+    layout: "/admin",
+    path: "default",
+    icon: < MdHome className = "h-6 w-6" / > ,
+    component: < MainDashboard / > ,
+  },
+  {
+    name: "Servicios",
+    layout: "/admin",
+    path: "services",
+    icon: < MdOutlineShoppingCart className = "h-6 w-6" / > ,
+    component: < Services / > ,
+    secondary: true,
+  },
+  {
+    name: "Reportes",
+    layout: "/admin",
+    icon: < MdBarChart className = "h-6 w-6" / > ,
+    path: "reports",
+    component: < Reports / > ,
+  },
+  {
+    name: "Perfil",
+    layout: "/admin",
+    path: "profile",
+    icon: < MdPerson className = "h-6 w-6" / > ,
+    component: < Profile / > ,
+  },
+  {
+    name: "Salir",
+    layout: "/auth",
+    path: "sign-in",
+    icon: < MdLock className = "h-6 w-6" / > ,
+    component: < SignIn / > ,
+  },
+];
+export default routes;
